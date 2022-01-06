c77fdf40a7cbd4add41d252abd4aa679b069d048
Alicja Niewiadomska <alicja.niewiadomska@swmansion.com>
2022-01-06 13:55:31 +0100

    Add lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Circle, Layer, Rect, Stage } from 'react-konva';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect
+          x={20}
+          y={50}
+          width={100}
+          height={100}
+          fill="red"
+          shadowBlur={10}
+        />
+        <Circle x={200} y={100} radius={50} fill="green" />
+      </Layer>
+      </Stage>
     </div>
   );
 }
